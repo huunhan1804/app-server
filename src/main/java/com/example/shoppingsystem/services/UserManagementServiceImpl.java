65029e99b0493aa255d1d19a9ca6a368ce0e7bf4
huunhan1804 <huunhan180403@gmail.com>
2025-08-09 20:06:55 +0700

    Update UserManagementServiceImpl.java

--- src/main/java/com/example/shoppingsystem/services/UserManagementServiceImpl.java
+++ src/main/java/com/example/shoppingsystem/services/UserManagementServiceImpl.java
@@ -354,7 +354,7 @@ public class UserManagementServiceImpl implements UserManagementService {
                 .createdDate(account.getCreatedDate())
                 .lastLogin(account.getLastLogin())
                 .businessName(agencyInfo.map(AgencyInfo::getShopName).orElse("Chưa đăng ký"))
-                .approvalStatus(account.getApprovalStatus().toString())
+                .approvalStatus(account.getApprovalStatus().getStatusCode())
                 .totalRevenue(totalRevenue)
                 .storeRating(agencyInfo.map(info -> 0.0).orElse(0.0)) // Default rating
                 .totalReviews(agencyInfo.map(info -> 0).orElse(0)) // Default reviews
