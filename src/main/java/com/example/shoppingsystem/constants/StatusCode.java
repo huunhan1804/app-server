f2e8c25cef30f50453677e5b60d3ff9e70a4f9a9
AnhDo03 <anhdo.06052003@gmail.com>
2025-08-14 13:34:46 +0700

    status commit

--- src/main/java/com/example/shoppingsystem/constants/StatusCode.java
+++ src/main/java/com/example/shoppingsystem/constants/StatusCode.java
@@ -1,8 +1,8 @@
 package com.example.shoppingsystem.constants;
 
 public class StatusCode {
-    public static final String STATUS_PENDING = "pending";
-    public static final String STATUS_APPROVED = "approved";
-    public static final String STATUS_REJECTED = "rejected";
+    public static final String STATUS_PENDING = "PENDING";
+    public static final String STATUS_APPROVED = "APPROVED";
+    public static final String STATUS_REJECTED = "REJECTED";
     public static final String STATUS_NOT_REGISTERED = "NOT_REGISTERED";
 }
